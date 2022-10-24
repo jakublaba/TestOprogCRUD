f0e6029bcd90ac38fccfe788a4b99547480af187
mmichalski <legeof008@gmail.com>
2022-10-24 19:08:13 +0200

    add resource loader, fixed initial test

--- src/test/java/controller/CrudControllerTest.java
+++ src/test/java/controller/CrudControllerTest.java
@@ -1,70 +1,65 @@
 package controller;
 
 import model.User;
+import org.dbunit.Assertion;
 import org.dbunit.IDatabaseTester;
 import org.dbunit.JdbcDatabaseTester;
 import org.dbunit.PropertiesBasedJdbcDatabaseTester;
 import org.dbunit.dataset.IDataSet;
 import org.dbunit.dataset.ITable;
 import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 import java.io.FileInputStream;
+import java.io.InputStream;
 
 import static org.dbunit.Assertion.assertEquals;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class CrudControllerTest {
-    private static final String JDBC_DRIVER = "org.h2.Driver";
-    private static final String DB_UNIT_DRIVER_CONNECTION_URL = "jdbc:h2:mem:default;DB_CLOSE_DELAY=-1;init=runscript from '/home/maciek/Templates/Studia/testy/TestOprogCRUD/src/test/resources/users_schema.sql'";
-    private static final String DB_UNIT_CONNECTION_URL_SHORT = "jdbc:h2:mem:default";
-    private static final String MOCK_DATABASE_DIR = "/home/maciek/Templates/Studia/testy/TestOprogCRUD/src/test/resources/users_mock.xml";
-    private final static String USERNAME = "sa";
-    private final static String PASSWORD = "sa";
-    private final static String TABLE_NAME = "USERS";
+    private static final String JDBC_DRIVER_SQREL = "org.postgresql.Driver";
+    private static final String DB_UNIT_CONNECTION_SGREL_SHORT = "jdbc:postgresql://localhost:5432/users";
+    private static final String MOCK_DATABASE_DIR = "/users_mock.xml";
+    private final static String USERNAME = "root";
+    private final static String PASSWORD = "root";
+    private final static String TABLE_NAME = "users";
     private ITable defaultTable;
     private IDatabaseTester databaseTester;
 
     private CrudController controller;
-
-    protected CrudControllerTest() {
-        System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_DRIVER_CLASS, JDBC_DRIVER);
-        System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_CONNECTION_URL, DB_UNIT_CONNECTION_URL_SHORT);
-        System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_USERNAME, USERNAME);
-        System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_PASSWORD, PASSWORD);
-
-    }
-
+    private InputStream inputStream;
     @BeforeAll
     public void setup() throws Exception {
-        System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_DRIVER_CLASS, JDBC_DRIVER);
-        System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_CONNECTION_URL, DB_UNIT_CONNECTION_URL_SHORT);
+        System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_DRIVER_CLASS, JDBC_DRIVER_SQREL);
+        System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_CONNECTION_URL, DB_UNIT_CONNECTION_SGREL_SHORT);
         System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_USERNAME, USERNAME);
         System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_PASSWORD, PASSWORD);
-        databaseTester = new JdbcDatabaseTester(JDBC_DRIVER, DB_UNIT_DRIVER_CONNECTION_URL, USERNAME, PASSWORD);
+
+        inputStream = this.getClass().getResourceAsStream(MOCK_DATABASE_DIR);
+        databaseTester = new JdbcDatabaseTester(JDBC_DRIVER_SQREL, DB_UNIT_CONNECTION_SGREL_SHORT, USERNAME, PASSWORD);
         IDataSet dataSet = getDataSet();
         defaultTable = dataSet.getTable(TABLE_NAME);
 
         databaseTester.setDataSet(dataSet);
 
         databaseTester.onSetup();
-        controller = new CrudController(DB_UNIT_CONNECTION_URL_SHORT,USERNAME,PASSWORD);
+        controller = new CrudController(DB_UNIT_CONNECTION_SGREL_SHORT, USERNAME, PASSWORD);
     }
 
     protected IDataSet getDataSet() throws Exception {
         return new FlatXmlDataSetBuilder().build(
-                new FileInputStream(MOCK_DATABASE_DIR));
+                inputStream);
     }
 
     @Test
     public void create_WithUser_Correct() throws Exception {
         User correctUser = new User("Test");
         controller.create(correctUser);
-        ITable resultingTable = databaseTester.getConnection().createQueryTable("users","SELECT * FROM users");
-        assertEquals(resultingTable,defaultTable);
-
+        ITable resultingTable = databaseTester.getConnection().createQueryTable(TABLE_NAME, "SELECT * FROM " + TABLE_NAME);
+        Assertions.assertEquals(resultingTable.getRowCount(), defaultTable.getRowCount() + 1);
     }
 
     @Test
--- src/test/resources/users_data.sql
+++ src/test/resources/users_data.sql
@@ -1,3 +1,8 @@
+DROP TABLE IF EXISTS users;
+CREATE TABLE IF NOT EXISTS users (
+                                     id SERIAL PRIMARY KEY,
+                                     username VARCHAR(31) NOT NULL
+);
 INSERT INTO users VALUES (DEFAULT, 'Krabelard');
 INSERT INTO users VALUES (DEFAULT, 'Gordon');
 INSERT INTO users VALUES (DEFAULT, 'Sysy');
@@ -6,5 +11,4 @@ INSERT INTO users VALUES (DEFAULT, 'Gniok');
 INSERT INTO users VALUES (DEFAULT, 'Craig');
 INSERT INTO users VALUES (DEFAULT, 'MrZaroweczka');
 INSERT INTO users VALUES (DEFAULT, 'Grypsztals');
-INSERT INTO users VALUES (DEFAULT, 'Szniok');
 INSERT INTO users VALUES (DEFAULT, 'Dziok');
