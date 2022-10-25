e8ef4a85e1d48a7861dbbe6576ed711e1f92468c
michalskim <72550341+legeof008@users.noreply.github.com>
2022-10-25 22:14:17 +0200

    script fix xd

--- docker_setup.sh
+++ docker_setup.sh
@@ -9,5 +9,5 @@ fi
 docker stop to-db 2> /dev/null
 docker rm to-db 2> /dev/null
 docker run --name to-db -e POSTGRES_PASSWORD=admin --rm -p 5431:5432 -d postgres &&
-docker cp src/test/resources/users_data.sql to-db:/docker-entrypoint-initdb.d/dump.sql && sleep 2 &&
+docker cp src/test/resources/users_schema.sql to-db:/docker-entrypoint-initdb.d/dump.sql && sleep 2 &&
 docker exec --user postgres -it to-db psql postgres postgres -f /docker-entrypoint-initdb.d/dump.sql
